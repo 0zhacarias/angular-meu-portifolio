4f8a12fd28eb0a78ad043fa2572fb630d07336e4
Zacarias Mutue. Milonga <zacarias.milonga@intra.mutue.co.ao>
2024-03-05 15:19:56 +0100

    Criação da pagina principal do portfolio

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import Typed from 'typed.js';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    const options = {
+      strings: ['Hello', 'World'],
+      typeSpeed: 50,
+      loop: true
+    };
+
+    const typed = new Typed('#typed', options);
   }
 
 }
