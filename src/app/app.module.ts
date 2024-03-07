93c437274281985bc5e21f59462cd936f6ccaaae
Zacarias Mutue. Milonga <zacarias.milonga@intra.mutue.co.ao>
2024-03-07 17:16:16 +0100

    Criação de carde para mostrar as tecnologias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent
   ],
+
+
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
